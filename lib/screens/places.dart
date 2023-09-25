d1ae2f5aac9243fe43a4285c89a9360c38dcbff6
Sainath S <sainath.shanigaram444@gmail.com>
2023-09-25 19:46:00 +0530

    InitialCommit

---
+++ lib/screens/places.dart
@@ -0,0 +1,22 @@
+import 'package:favorite_places/screens/add_place.dart';
+import 'package:favorite_places/widgets/places_list.dart';
+import 'package:flutter/material.dart';
+
+class PlaceListScreen extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+         title: const Text("Your Places"),
+        actions: [
+          IconButton(onPressed: (){
+            Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=> const AddPlaceScreen()));
+          }, icon: const Icon(Icons.add))
+        ],
+      ),
+      body: const PlaceList(places: []),
+    );
+  }
+
+}
